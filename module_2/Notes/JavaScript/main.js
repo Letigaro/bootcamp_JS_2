d9648163343e0d13afd0c8874deeb363bd1b045e
Letigaro <113176418+Letigaro@users.noreply.github.com>
2024-09-13 14:07:36 +0200

    module - 2 (#3)

---
+++ module_2/Notes/JavaScript/main.js
@@ -0,0 +1,20 @@
+console.log("Hola JavaScript");
+console.log(2+3);
+console.log((2*6) + "€ en total");
+
+console.log(Math.PI*4*5 + " es el area del círculo");
+
+console.log(true);
+
+console.log(
+    {
+        nif:'8945945864',
+        name: 'Pepe',
+        lastName: 'Perez',
+        direction:{
+            street: 'street fjrifr',
+            city: 'vnuehrie',
+            zipCode:'85404'
+        }
+    }
+);
